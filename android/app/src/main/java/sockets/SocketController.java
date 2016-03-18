305672a43f61d024f0b7a8b479ca3e02a8954192
marcin <majewskimarcin@outlook.com>
2016-03-19 00:27:04 +0100

    -Cleaning build.gradle -SocketController mock

--- android/app/src/main/java/sockets/SocketController.java
+++ android/app/src/main/java/sockets/SocketController.java
@@ -2,14 +2,11 @@ package sockets;
 
 import io.socket.client.IO;
 import io.socket.client.Socket;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 import java.net.URISyntaxException;
 import java.util.List;
 
 class SocketController {
-	private final static Logger logger = LogManager.getLogger(SocketController.class);
 	private Socket socket;
 
 	public SocketController(String url) throws URISyntaxException {
@@ -18,7 +15,6 @@ class SocketController {
 		opts.reconnection = false;
 		socket = IO.socket(url, opts);
 		socket.connect();
-		logger.info(String.format("Connecting to socket with url %s", url));
 	}
 
 	public void performMovement(Movement movement, List<String> arguments) {

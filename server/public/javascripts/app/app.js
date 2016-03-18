43dfe0c842e366e8f2e524f3c30e2f27b4edb8aa
Mateusz Kmiecik <mk.chomik@gmail.com>
2016-03-18 23:00:03 +0100

    added uirouter boilerplate

--- server/public/javascripts/app/app.js
+++ server/public/javascripts/app/app.js
@@ -1,5 +1,14 @@
 'use strict';
 
-var eBox = angular.module('ebox', [])
+var eBox = angular.module('ebox', ['ui.router']);
 
+eBox.config(function ($stateProvider, $urlRouterProvider) {
 
+    $urlRouterProvider.otherwise('/')
+
+    $stateProvider.state('home', {
+        url: '/',
+        controller: 'HomeController',
+        templateUrl: 'javascripts/app/home/home.html'
+    });
+});

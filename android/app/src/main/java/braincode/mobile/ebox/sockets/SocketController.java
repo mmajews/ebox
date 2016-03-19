bbfd7c77b94c47498a53f80b6c44778b5396b74e
marcin <majewskimarcin@outlook.com>
2016-03-19 15:43:11 +0100

    Now log is being shown why using

--- android/app/src/main/java/braincode/mobile/ebox/sockets/SocketController.java
+++ android/app/src/main/java/braincode/mobile/ebox/sockets/SocketController.java
@@ -1,24 +1,31 @@
 package braincode.mobile.ebox.sockets;
 
 import android.util.Log;
-
-import java.net.URI;
-import java.util.Date;
-
+import android.widget.TextView;
+import braincode.mobile.ebox.R;
 import braincode.mobile.ebox.gesture.GestureEvent;
 import io.socket.client.IO;
 import io.socket.client.Socket;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.net.URI;
+import java.util.Date;
 
 public class SocketController {
 
     private static final int EVENT_GAP = 20;
 
     private Socket socket;
+    private Handler handler = new Handler();
+    private TextView currentSendTextView;
 
     private long previous;
 
-    public SocketController(URI uri) {
+    public SocketController(Activity activity,URI uri) {
         this.socket = IO.socket(uri);
+        currentSendTextView = (TextView) activity.findViewById(R.id.currentSendTextView);
+
     }
 
     public void connect() {
@@ -34,6 +41,12 @@ public class SocketController {
         if (timestamp - previous > EVENT_GAP) {
             Object data = gestureEvent.getData();
             Log.d("padEvent", "TIMESTAMP:" + new Date().getTime() + ", data: " + data);
+            try {
+                JSONObject json = new JSONObject(gestureEvent.getData().toString()); // Convert text to object
+                currentSendTextView.setText(json.toString(4));
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
             socket.emit("padEvent", data);
             previous = timestamp;
         }

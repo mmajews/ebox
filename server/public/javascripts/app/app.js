eab540cf63bcc6b135e094a11cb764ee3c038290
Mateusz Kmiecik <mk.chomik@gmail.com>
2016-03-19 10:53:46 +0100

    cursor working, add styling

--- server/public/javascripts/app/app.js
+++ server/public/javascripts/app/app.js
@@ -7,41 +7,61 @@ ebox.config(function ($stateProvider, $urlRouterProvider) {
     $urlRouterProvider.otherwise('/')
 
     $stateProvider.state('home', {
-        url: '/',
-        controller: 'HomeController',
-        templateUrl: 'javascripts/app/home/home.html'
-    })
-    .state('game', {
-        url: '/game',
-        controller: 'GameController',
-        templateUrl: 'javascripts/app/game/game.html'
-    })
-    .state('pong', {
-        url: '/pong',
-        templateUrl: 'javascripts/app/pong/game.html'
-    })
-    .state('flappyBird', {
-        url: '/flappy_bird',
-        templateUrl: 'javascripts/app/flappy_bird/game.html'
-    })
+            url: '/',
+            templateUrl: 'javascripts/app/home/home.html',
+            controller: function ($rootScope, SocketService, $window) {
+                $rootScope.hideCursor = false;
+
+                SocketService.on('onScroll', function (data) {
+                    var offset = $('#cursor').offset();
+                    var newOffset = {top: offset.top - data.distanceX*2, left: offset.left-data.distanceY*2}
+                    if(newOffset.top < 0){
+                        newOffset.top = 0;
+                    }
+                    if(newOffset.left < 0){
+                        newOffset.left = 0;
+                    }
+
+                    if(newOffset.top > $window.innerHeight){
+                        newOffset.top = $window.innerHeight;
+                    }
+                    if(newOffset.left > $window.innerWidth){
+                        newOffset.left = $window.innerWidth;
+                    }
+
+                    $('#cursor').offset(newOffset);
+                });
+
+            }
+        })
+        .state('game', {
+            url: '/game',
+            controller: 'GameController',
+            templateUrl: 'javascripts/app/game/game.html'
+        })
+        .state('pong', {
+            url: '/pong',
+            templateUrl: 'javascripts/app/pong/game.html',
+            controller: function ($rootScope) {
+                $rootScope.hideCursor = true;
+            }
+        })
+        .state('flappyBird', {
+            url: '/flappy_bird',
+            templateUrl: 'javascripts/app/flappy_bird/game.html'
+        })
 });
 
-ebox.run(function ($document, $rootScope, $state) {
+ebox.run(function ($document, $rootScope, $state, SocketService) {
+
+    SocketService.noop();
     setTimeout(function () {
         $('.overlay').fadeOut();
     }, 1000);
 
-    $document.bind('keydown', function (e) {
-        if(e.which == 37){
-            $rootScope.emit('keydown:left');
-        } else if(e.which == 39){
-            $rootScope.emit('keydown:right');
-        }
-    });
-
     $document.bind('mousemove', function (e) {
         var x = e.pageX, y = e.pageY;
-        $('#cursor').offset({left: x+1, top: y+1});
+        $('#cursor').offset({left: x + 1, top: y + 1});
     });
 
     $rootScope.goTo = function (state) {

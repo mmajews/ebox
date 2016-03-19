62c8de7204dc000d5d0fb38b772490a29727920b
Lukasz Gajewski <lukasz.gajewski94@gmail.com>
2016-03-19 01:01:28 +0100

    changed layot to activity_hello

--- android/app/src/main/java/braincode/mobile/ebox/HelloActivity.java
+++ android/app/src/main/java/braincode/mobile/ebox/HelloActivity.java
@@ -6,9 +6,7 @@ package braincode.mobile.ebox;
 
 import android.app.Activity;
 import android.os.Handler;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
@@ -34,7 +32,7 @@ public class HelloActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_hello);
 
         messageText = (EditText) findViewById(R.id.messageText);
 

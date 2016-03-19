a60e9cf55a56d46b680d2776926345dbe0be76e0
Konrad Marzec <konrad.marzec@swmansion.com>
2016-03-19 04:55:51 +0100

    qrcode

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
+var QRCode = require('qrcode');
+var ip;
+require('dns').lookup(require('os').hostname(), function (err, addr, fam) {
+  ip = addr;
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.sendfile('public/index.html');
+});
+
+router.get('/qrcode', function(req, res, next) {
+    console.log(ip);
+    QRCode.draw(ip + ':3000', function(err,canvas){
+        stream = canvas.pngStream();
+        res.setHeader('Content-type', 'image/png');
+        stream.pipe(res);
+    });
+
 });
 
 module.exports = router;
